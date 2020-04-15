b33425d5d2e6208f66b3561a47d39b304a5ca754
Emmet Murphy <emmet@emurphy.net>
2020-04-15 16:34:06 -0700

    interval is supported in Snowflake: https://docs.snowflake.com/en/sql-reference/data-types-datetime.html#interval-constants

--- translator/redshift_to_snowflake.py
+++ translator/redshift_to_snowflake.py
@@ -9,9 +9,7 @@ from translator.translate_token import TranslateToken
 def redshift_to_snowflake(token: TranslateToken) -> None:
     try:
 
-        if token.matches('interval'):
-            convert_interval(token)
-        elif token.is_function():
+        if token.is_function():
             if token.matches('date'):
                 token.set('TO_DATE')
             elif token.startswith('convert_timezone'):
@@ -21,79 +19,9 @@ def redshift_to_snowflake(token: TranslateToken) -> None:
         elif token.matches('#'):
             token.set('num')
 
-
     except ValueError as e:
         print(token.get_statement())
         raise e
-
-
-def convert_interval(token):
-    """
-    Converts the Postgresql/Redshift INTERVAL construct to ANSI SQL functions or operations, including:
-      - dateadd(<unit>, <increment>, <timestamp>)
-      - datediff(timestamp1, timestamp2) < <increment> <unit>
-    :param token: The INTERVAL token
-    :return: None
-    """
-    date_var = date_var_comp = None
-    op = None
-    new_children = []
-    stack = []
-    # from the INTERVAL token we backtrack to understand the context, then re-assemble the tokens accordingly
-    if token.parent and token.parent.children:
-        for i, sibling in enumerate(token.parent.children):
-            if sibling == token:
-                prev = pop_beyond_whitespace(stack)
-                op = prev.value()
-                date_var = pop_beyond_whitespace(stack)
-                if date_var.is_comparison(): # e.g. date1 >= date2 - interval '5 day'
-                    stack.extend(date_var.children[:-1])
-                    date_var = date_var.children[-1]
-                elif op in ['>', '>=', '<', '<=']:  # e.g. date1 - date2 >= interval '4 hour'
-                    date_op = [o for o in date_var.children if o in ['+', '-']]
-                    if date_op == '+':
-                        print(f"Migration of 'interval' with '+' in {token.get_statement()} not yet implemented")
-                        return
-                    date_var, date_var_comp = (date_var.children[0], date_var.children[-1])
-                new_children.extend(stack)
-            elif not date_var:
-                stack.append(sibling)
-            elif sibling.is_whitespace() and not token.has_changed():
-                pass
-            elif sibling.is_literal():
-                parts = sibling.value().replace("'", '').strip().split(' ')
-                if len(parts) == 1:  # e.g. -60DAY
-                    num = parts[0]
-                    num += ''.join([d for d in parts[1:] if d.isdigit()])
-                    unit = parts[len(num) - 1:]
-                elif len(parts) == 2:  # e.g. -60 DAY
-                    num, unit = parts
-                elif len(parts) == 3:  # e.g. - 60 DAY
-                    num = parts[-1]
-                    unit = ''.join(parts[-1:])
-                else:
-                    print(f"Handling interval in CASE not yet implemented, in {token.get_statement()}")
-                    return
-
-                if op in ['+', '-']:
-                    if num.startswith('-'):
-                        if op == '+':
-                            op = '-'
-                        else:
-                            op = '+'
-                        num = num[1:]
-                    token.set(f"DATEADD({unit}, {op}{num}, {date_var.value()})")
-                elif date_var_comp is not None:
-                    token.set(f"DATEDIFF({unit}, {date_var.value()}, {date_var_comp.value()}) {op} {num}")
-                new_children.append(token)
-                new_children.extend(token.parent.children[i + 1:])
-                break
-            else:
-                new_children.append(sibling)
-
-        token.parent.children[:] = new_children
-    else:
-        print(f"ignoring 'interval' in {token.get_statement()}")
 
 
 def pop_beyond_whitespace(stack: List[TranslateToken]) -> TranslateToken:
