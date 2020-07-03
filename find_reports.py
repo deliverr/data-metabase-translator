9ad6c051fe8bb2c2af218ad9be85653ad7dca37c
Emmet Murphy <emmet@deliverr.com>
2020-07-03 16:41:53 -0700

    Find reports that contain a string (#6)

---
+++ find_reports.py
@@ -0,0 +1,35 @@
+"""
+Reads queries from Metabase and finds reports that contain a string
+"""
+from metabase import Properties, ReportCardRepo
+import sqlparse
+
+
+def main():
+    props = Properties(open('metabase.yaml', 'r'))
+
+    repo = ReportCardRepo(props)
+    report_cards = repo.fetchall()
+    print(f"Retrieved {len(report_cards)} native queries")
+
+    table = 'find.me'
+    reports = []
+    for report_card in report_cards:
+        sql = report_card.dataset_query['native']['query']
+        statements = sqlparse.parse(sql)
+        for statement in statements:
+            if references(statement, table):
+              print(f"{report_card}")
+              reports.append(report_card)
+
+    print(f"Found {len(reports)} reports: {', '.join(sorted([str(r.id) for r in reports]))}")
+
+
+def references(statement, table):
+    if table in statement.value.lower():
+        return True
+    return False
+
+
+if __name__ == '__main__':
+    main()
